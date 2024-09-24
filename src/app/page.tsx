0c37109edda4c1a19766c44dabfa4c9e9265a07b
salmanhaneef12 <salmanhanif80450@gmail.co>
2024-09-24 19:48:42 +0500

    Navbar componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
+import { main } from "framer-motion/client";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl text-center text-red-600">This is my first next app</h1>
- 
+    <main className="min-h-screen bg-black/[0.96] antialiased
+    bg-grid-white/[0.02]">
+      <h1 className="text-4xl text-center ">
+        This is my first next app
+      </h1>
+    </main>
   );
 }
--- src/app/practice/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function practice () {
-    return (
-        <h1>this is my page</h1>
-    );
-}
